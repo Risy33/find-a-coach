c8a14d821306fe70e82465ee2666682e10f274b5
risit500 <dario.risitano@kpn.com>
2024-03-29 17:15:16 +0100

    adding coaches to vuex

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -2,7 +2,7 @@
   <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="firstname">Firstname</label>
-      <input type="text" id="firstname"  v-model="firstName"/>
+      <input type="text" id="firstname" v-model="firstName" />
     </div>
     <div class="form-control">
       <label for="lastname">Lastname</label>
@@ -10,56 +10,56 @@
     </div>
     <div class="form-control">
       <label for="description">Description</label>
-      <textarea  id="description" rows="5" v-model.trim="description"></textarea>
+      <textarea id="description" rows="5" v-model.trim="description"></textarea>
     </div>
     <div class="form-control">
       <label for="rate">Hourly Rate</label>
-      <input  id="description" type="number" v-model.number="rate"/>
+      <input id="description" type="number" v-model.number="rate" />
     </div>
     <div class="form-control">
       <h3>Areas of expertiese</h3>
       <div>
-      <input type="checkbox" id="frontend" value="frontend" v-model="areas"/>
-      <label for="frontend">Frontend Development</label>
+        <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
+        <label for="frontend">Frontend Development</label>
+      </div>
+      <div>
+        <input type="checkbox" id="backend" value="backend" v-model="areas" />
+        <label for="backend">Backend Development</label>
+      </div>
+      <div>
+        <input type="checkbox" id="career" value="career" v-model="areas" />
+        <label for="career">Career Advisory</label>
+      </div>
     </div>
-    <div>
-      <input type="checkbox" id="backend" value="backend" v-model="areas"/>
-      <label for="backend">Backend Development</label>
-    </div>
-    <div>
-      <input type="checkbox" id="career" value="career" v-model="areas"/>
-      <label for="career">Career Advisory</label>
-    </div>
-  </div>
-  <base-button>Register</base-button>
+    <base-button>Register</base-button>
   </form>
 </template>
 
-
 <script>
 export default {
-    data(){
-        return {
-            firstName:'',
-            lastName:'',
-            description:'',
-            rate:'',
-            areas:[]
-        }
+    emit:['save-data'],
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      description: '',
+      rate: '',
+      areas: [],
+    };
+  },
+  methods: {
+    submitForm() {
+      const formData = {
+        first: this.firstName,
+        last: this.lastName,
+        desc: this.description,
+        rate: this.rate,
+        areas: this.areas,
+      };
+      this.$emit('save-data', formData);
     },
-    methods:{
-        submitForm() {
-            const formData = {
-                first:this.firstName,
-                last:this.lastName,
-                desc:this.description,
-                rate:this.rate,
-                areas:this.areas
-            }
-            console.log(formData)
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
